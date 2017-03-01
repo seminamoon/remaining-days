8e3868c32e2f7ca74d7e375c03ef7a24cbcdf160
moon.semina <semina.moon@gmail.com>
2017-03-01 20:10:03 +0900

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name remainingDaysApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the remainingDaysApp
+ */
+angular.module('remainingDaysApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
